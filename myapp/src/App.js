b8c947a65313123b6f0fce88f2df5b8072d4c5bc
guy <guy@example.com>
2021-05-19 05:12:55 -0700

    basic signup page

---
+++ myapp/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import SignUp from "./components/SignUp"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+  <SignUp/>
+    </div>
+  );
+}
+
+export default App;
